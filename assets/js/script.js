5c021a20f05aeda59ad2a9b5075b90ddb682902b
Enol Santurio <enolsantufer@gmail.com>
2025-12-10 21:24:06 +0100

    arreglo anterior subido

--- assets/js/script.js
+++ assets/js/script.js
@@ -12,54 +12,52 @@ const buscar = document.getElementById("buscar");
 
 const pedirProducto = () => prompt("Introduce el producto.");
 
-function pedirStock (){
+function pedirStock() {
   const pedir = prompt("Introduce la cantidad de producto.");
-  if(!pedir) return null;
+  if (!pedir) return null;
 
   const numeroStock = parseInt(pedir);
-  if(isNaN(numeroStock)){
+  if (isNaN(numeroStock)) {
     alert("La cantidad tiene que ser un numero.");
     return null;
   }
   return numeroStock;
-} 
+}
 
 //CARGA DEL LOCAL STORAGE:
 
-const inventarioSave = JSON.parse(localStorage.getItem("inventario"));  // Esto sirve para recuperar del localStorage lo guardado y que lo muestre en el Dom al abrir la pagina.
-if(inventarioSave){
-    inventario = inventarioSave;
-    inventario.productos.forEach(articulo => {
-      if(!articulo.nombre) return; // Evita que carge articulos sin valor definido.
-      actualizarDomAgregar(articulo.nombre, articulo.cantidad);
-    })
-
+const inventarioSave = JSON.parse(localStorage.getItem("inventario")); // Esto sirve para recuperar del localStorage lo guardado y que lo muestre en el Dom al abrir la pagina.
+if (inventarioSave) {
+  inventario = inventarioSave;
+  inventario.productos.forEach((articulo) => {
+    if (!articulo.nombre) return; // Evita que carge articulos sin valor definido.
+    actualizarDomAgregar(articulo.nombre, articulo.cantidad);
+  });
 }
-
 
 //FUNCIONES:
 
-      //AGREGAR:
+//AGREGAR:
 
 function guardarLocalStorage() {
-  localStorage.setItem("inventario", JSON.stringify(inventario));//Guarda en localStorage la lista.
+  localStorage.setItem("inventario", JSON.stringify(inventario)); //Guarda en localStorage la lista.
 }
 
 function actualizarDomAgregar(producto, stock) {
-  if(!producto) return;// Evita  articulos sin valor definido.
+  if (!producto) return; // Evita  articulos sin valor definido.
   const ul = document.getElementById("lista");
   const li = document.createElement("li");
   li.textContent = ` Nombre: ${producto}, Stock: ${stock}.`; //Esto modifica el DOM.
   li.dataset.producto = producto.toLowerCase(); // Esto crea un identificador del li con el nombre del producto.
-  ul.appendChild(li);//Esto lo añade al HTML.
+  ul.appendChild(li); //Esto lo añade al HTML.
 }
 
 function añadirProducto() {
   let producto = pedirProducto();
-  if (!producto) return;// Evita  articulos sin valor definido.
+  if (!producto) return; // Evita  articulos sin valor definido.
 
   let stock = pedirStock();
-  if(stock === null) return;
+  if (stock === null) return;
   inventario.productos.push({ nombre: producto, cantidad: stock });
 
   console.log(`Producto ${producto} con ${stock} de stock actualizado.`);
@@ -69,27 +67,27 @@ function añadirProducto() {
   guardarLocalStorage();
 }
 
-agregar.addEventListener("click", añadirProducto);//Evento de agregar.
+agregar.addEventListener("click", añadirProducto); //Evento de agregar.
 
-
-      //BUSCAR:
+//BUSCAR:
 
 function actualizarDomBuscar(producto) {
-  const li = document.querySelector(`[data-producto="${producto.toLowerCase()}"]`);
-  if(li){
+  const li = document.querySelector(
+    `[data-producto="${producto.toLowerCase()}"]`
+  );
+  if (li) {
     li.style.fontWeight = "bold";
-    li.style.fontStyle= "italic";
-    li.style.marginLeft= "90px";
+    li.style.fontStyle = "italic";
+    li.style.marginLeft = "90px";
   }
 }
-
 
 function mirarProducto() {
   let producto = pedirProducto();
   if (!producto) return;
-  
-  producto = producto.trim();   
-  if (producto === "") return;// Evita  articulos sin valor definido.
+
+  producto = producto.trim();
+  if (producto === "") return; // Evita  articulos sin valor definido.
 
   let busqueda = inventario.productos.find(
     (p) => p.nombre.toLowerCase() === producto.toLowerCase()
@@ -97,15 +95,12 @@ function mirarProducto() {
   if (busqueda) {
     alert(`El producto ${busqueda.nombre} tiene ${busqueda.cantidad}`);
     actualizarDomBuscar(busqueda.nombre);
-
-    
   } else {
     alert(`No existe el producto ${producto} de stock.`);
   }
-
 }
 
-buscar.addEventListener("click", mirarProducto);//Evento de busqueda.
+buscar.addEventListener("click", mirarProducto); //Evento de busqueda.
 
 //ELIMINAR:
 
@@ -113,7 +108,7 @@ function quitarProducto() {
   let producto = pedirProducto();
   if (!producto) return;
 
-  producto = producto.trim();   
+  producto = producto.trim();
   if (producto === "") return;
 
   let confirma = confirm(`¿Seguro quires borrar el producto ${producto}?`);
@@ -122,17 +117,18 @@ function quitarProducto() {
       (p) => p.nombre && p.nombre.toLowerCase() === producto.toLowerCase()
     );
     if (ubicacion !== -1) {
-
-      const li = document.querySelector(`[data-producto="${producto.toLowerCase()}"]`);//Elimina lo guardado en el Dom.
-      if(li){
+      const li = document.querySelector(
+        `[data-producto="${producto.toLowerCase()}"]`
+      ); //Elimina lo guardado en el Dom.
+      if (li) {
         li.remove();
       }
 
       inventario.productos.splice(ubicacion, 1);
       alert(`El producto ${producto} a sido borrado del inventario.`);
-      guardarLocalStorage()
+      guardarLocalStorage();
     }
   }
 }
 
-eliminar.addEventListener("click",quitarProducto);//Evento de eliminar.
+eliminar.addEventListener("click", quitarProducto); //Evento de eliminar.
